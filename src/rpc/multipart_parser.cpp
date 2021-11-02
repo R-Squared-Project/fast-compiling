71eed7eb298c131dee61fcc9f37ee12bae8f4ece
GoodWien <mailto:89968171+GoodWien@users.noreply.github.com>
2021-11-02 12:26:38 +0200

    #64 Update multipart file saver by saved files list at parameters

--- src/rpc/multipart_parser.cpp
+++ src/rpc/multipart_parser.cpp
@@ -1,6 +1,7 @@
 #include <fc/rpc/multipart_parser.hpp>
 
 #include <fc/io/json.hpp>
+#include <fc/reflect/variant.hpp>
 
 #include "multipartparser.h"
 
@@ -19,6 +20,13 @@ typedef struct part {
     std::string body;
 } part;
 
+typedef struct part_file {
+    std::string name;
+    std::string path;
+    explicit part_file(const std::string& name, const std::string& path)
+        : name(name), path(path) {}
+} part_file;
+
 static multipartparser_callbacks g_callbacks;
 
 static bool             g_body_begin_called;
@@ -26,6 +34,7 @@ static std::string      g_header_name;
 static std::string      g_header_value;
 static std::list<part>  g_parts;
 static bool             g_body_end_called;
+static std::list<part_file>  g_files;
 
 static void init_globals()
 {
@@ -33,6 +42,7 @@ static void init_globals()
     g_header_name.clear();
     g_header_value.clear();
     g_parts.clear();
+    g_files.clear();
     g_body_end_called = false;
 }
 
@@ -93,48 +103,6 @@ static int on_body_end(multipartparser* /*parser*/)
     return 0;
 }
 
-void test_simple()
-{
-    multipartparser parser;
-
-#define BOUNDARY "simple boundary"
-#define PART0_BODY                                      \
-    "This is implicitly typed plain ASCII text.\r\n"    \
-    "It does NOT end with a linebreak."
-#define PART1_BODY                                      \
-    "This is explicitly typed plain ASCII text.\r\n"    \
-    "It DOES end with a linebreak.\r\n"
-#define BODY                                            \
-    "--" BOUNDARY "\r\n"                                \
-    "\r\n"                                              \
-    PART0_BODY                                          \
-    "\r\n--" BOUNDARY "\r\n"                            \
-    "Content-type: text/plain; charset=us-ascii\r\n"    \
-    "\r\n"                                              \
-    PART1_BODY                                          \
-    "\r\n--" BOUNDARY "--\r\n"                          \
-
-    init_globals();
-    multipartparser_init(&parser, BOUNDARY);
-
-    assert(multipartparser_execute(&parser, &g_callbacks, BODY, strlen(BODY)) == strlen(BODY));
-    assert(g_body_begin_called);
-    assert(g_parts.size() == 2);
-
-    part p0 = g_parts.front(); g_parts.pop_front();
-    assert(p0.headers.empty());
-    assert(p0.body.compare(PART0_BODY) == 0);
-
-    part p1 = g_parts.front(); g_parts.pop_front();
-    assert(p1.headers.size() == 1);
-    assert(p1.body == PART1_BODY);
-
-    assert(g_body_end_called);
-
-#undef BOUNDARY
-#undef BODY
-}
-
 multipart_parser::multipart_parser() {
 }
 
@@ -162,15 +130,59 @@ variant multipart_parser::parse_string( const std::string& message,  uint32_t ma
     multipartparser_init(&parser, boundary.c_str());
 
     std::string json_str = message;
+    bool json_found = false;
     assert(multipartparser_execute(&parser, &g_callbacks, message.c_str(), message.size()) == message.size());
     for (auto& gp : g_parts) {
-        if (gp.headers.count("Content-Disposition") 
-         && gp.headers["Content-Disposition"].find("name=\"json_data\"") != std::string::npos) {
-            json_str = gp.body;
-         }
+        if (gp.headers.count("Content-Disposition")) {
+            auto& content_disp_header = gp.headers["Content-Disposition"];
+
+            if (!json_found && content_disp_header.find("name=\"json_data\"") != std::string::npos) {
+                json_found = true;
+                json_str = gp.body;
+            } else {
+                auto file_name_start = content_disp_header.find("filename=\"");
+                if (file_name_start != std::string::npos) {
+                    file_name_start += 10;
+                    auto filename_end = content_disp_header.find_first_of("\"", file_name_start );
+                    if (filename_end != std::string::npos) {
+                        auto file_name = content_disp_header.substr(file_name_start, filename_end - file_name_start);
+                        g_files.emplace_back(file_name, gp.body);
+                    }
+                }
+            }
+        }
     }
 
     variant var = fc::json::from_string( json_str, fc::json::legacy_parser, max_depth );
+
+    if( var.is_object() && !g_files.empty()) {
+        variants files;
+        for (auto& f : g_files) {
+            mutable_variant_object file_obj;
+            file_obj("name", f.name);
+            file_obj("path", f.path);
+            files.push_back(file_obj);
+        }
+
+        variant_object var_obj = var.get_object();
+        mutable_variant_object mut_var_obj(var_obj);
+
+        if (var_obj.contains("params")) {
+            if (var_obj["params"].is_array()) {
+                variants params_obj = var_obj["params"].get_array();
+                params_obj.push_back(files);
+                mut_var_obj["params"] = params_obj;
+            }
+        } else {
+            variants params_obj;
+            params_obj.push_back(files);
+            mut_var_obj["params"] = params_obj;
+        }
+
+        auto json_string = fc::json::to_string(mut_var_obj);
+        return mut_var_obj;
+    }
+
     return var;
 }
 
